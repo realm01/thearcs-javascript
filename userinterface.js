b8ffdeebed45f5c33413ff71d9d7d22030d7eb5c
Anastassios Martakos <amartako@phys.ethz.ch>
2016-06-15 15:26:27 +0200

    Added License Information

--- userinterface.js
+++ userinterface.js
@@ -1,7 +1,22 @@
+/*
+* Created by: Anastassios Martakos
+*
+* Copyright 2013 Anastassios Martakos
+*
+* This program is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*/
+
 window.requestAnimFrame = (function () {
-	return window.requestAnimationFrame || 
-		window.webkitRequestAnimationFrame || 
-		window.mozRequestAnimationFrame || 
+	return window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
 		function (callback) {
 			window.setTimeout(callback, 1000 / fps);
 		};
